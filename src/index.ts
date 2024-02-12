3439436ecf7c6c53330afbf080dc2e03fb4f6006
Justin McBride <jstnmcbrd@outlook.com>
2024-02-12 04:34:53 -0800

    Upgrade to setup-node@v4 (#4)

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 import type { Linter } from 'eslint';
 
 import { javascript } from './javascript.js';
-import { typescript } from './typescript.js';
 import { react } from './react.js';
+import { typescript } from './typescript.js';
 
 interface Settings {
-	typescript?: boolean;
 	react?: boolean;
+	typescript?: boolean;
 }
 
 /** @returns Customizable ESLint configuration. */
 export default function eslintConfig(settings?: Settings): Linter.FlatConfig[] {
 	return [
 		...javascript(),
-		...(settings?.typescript ? typescript() : []),
 		...(settings?.react ? react() : []),
+		...(settings?.typescript ? typescript() : []),
 	];
 }
