d09fde834b694909f6ec203d7c053e066e277118
Justin McBride <jstnmcbrd@outlook.com>
2024-06-16 03:37:44 -0600

    Fix type shims still including flat config types

--- src/@types/eslint-plugin-react-hooks.d.ts
+++ src/@types/eslint-plugin-react-hooks.d.ts
@@ -2,7 +2,7 @@
 
 declare module 'eslint-plugin-react-hooks' {
 	import type { ESLint } from 'eslint';
-	const plugin: ESLint.Plugin & {
+	const plugin: Omit<ESLint.Plugin, 'configs'> & {
 		// eslint-plugin-react-hooks does not use FlatConfig yet
 		configs: Record<string, ESLint.ConfigData>;
 	};
--- src/@types/eslint-plugin-react.d.ts
+++ src/@types/eslint-plugin-react.d.ts
@@ -2,7 +2,7 @@
 
 declare module 'eslint-plugin-react' {
 	import type { ESLint } from 'eslint';
-	const plugin: ESLint.Plugin & {
+	const plugin: Omit<ESLint.Plugin, 'configs'> & {
 		// eslint-plugin-react does not use FlatConfig yet
 		configs: Record<string, ESLint.ConfigData>;
 	};
