3c64a506b67397598d487910442a82fbce6996e9
Justin McBride <jstnmcbrd@outlook.com>
2024-06-30 04:22:49 -0600

    Add issue links for type shims

--- src/@types/eslint-plugin-react-hooks.d.ts
+++ src/@types/eslint-plugin-react-hooks.d.ts
@@ -1,4 +1,5 @@
 // This is only a type-shim and is not meant to be a perfect representation
+// https://github.com/facebook/react/issues/30119
 
 declare module 'eslint-plugin-react-hooks' {
 	import type { ESLint } from 'eslint';
