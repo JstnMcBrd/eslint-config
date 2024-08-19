91061756177d603dabc8dd526fc25becd8fb0db7
Justin McBride <jstnmcbrd@outlook.com>
2024-08-19 02:00:41 -0600

    Update eslint-plugin-react types

--- src/@types/eslint-plugin-react.d.ts
+++ src/@types/eslint-plugin-react.d.ts
@@ -2,15 +2,19 @@
 // https://github.com/jsx-eslint/eslint-plugin-react/issues/3776
 
 declare module 'eslint-plugin-react' {
-	import type { ESLint } from 'eslint';
+	import type { ESLint, Linter } from 'eslint';
 	const plugin: {
 		deprecatedRules: ESLint.Plugin['rules'];
 		rules: ESLint.Plugin['rules'];
-		// Does not use FlatConfig yet
 		configs: {
 			'recommended': ESLint.ConfigData;
 			'all': ESLint.ConfigData;
 			'jsx-runtime': ESLint.ConfigData;
+			'flat': {
+				'recommended': Linter.FlatConfig;
+				'all': Linter.FlatConfig;
+				'jsx-runtime': Linter.FlatConfig;
+			};
 		};
 	};
 	export default plugin;
