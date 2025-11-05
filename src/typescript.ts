ffb0d80f91c35a015df22b68cad60bd78c92e647
Justin McBride <jstnmcbrd@outlook.com>
2025-11-05 01:35:46 -0700

    Use `projectService` for `typescript-eslint` typed linting (#150)

--- src/typescript.ts
+++ src/typescript.ts
@@ -9,28 +9,11 @@ export function typescript(): Config[] {
 			ignores: ['dist'],
 		},
 
+		// Type information
 		{
 			languageOptions: {
 				parserOptions: {
-					project: 'tsconfig.eslint.json',
-
-					/**
-					 * In order to use the new projectService feature, we would need to enable default project
-					 * for all files not included in the project's tsconfig. But `typescript-eslint` wants you
-					 * to only include specific files and blocks wide patterns. But we need to include
-					 * arbitrarily wide pattners because this eslint-config could be used in many projects with
-					 * diverse structures, so we cannot know what files to include in advance.
-					 *
-					 * `typescript-eslint` also enforces an 8-file limit, which would not be enough for projects
-					 * with many excluded files - for example, unit test `*.test.ts` files.
-					 *
-					 * Because of these limitations, we cannot use the new projectService feature.
-					 */
-
-					// projectService: {
-					// 	allowDefaultProject: ['**/*.{m,c,}{js,ts}{x,}'],
-					// 	defaultProject: 'tsconfig.json',
-					// },
+					projectService: true,
 				},
 			},
 		},
